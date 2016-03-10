e0bab92fb711d76429acaa4b6920b3d1d82b1f04
ravi0912 <ravirock0912@gmail.com>
2016-03-11 04:10:50 +0530

    Migration Page Database sequence changed

---
+++ database/migrations/2016_03_10_223739_create_pages_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('pages_category_id')->unsigned();
+            $table->foreign('pages_category_id')->references('id')->on('page_category')->onDelete('cascade');
+            $table->integer('pages_subcategory_id')->unsigned();
+            $table->foreign('pages_subcategory_id')->references('id')->on('page_subcategory')->onDelete('cascade');
+            $table->integer('type_id')->unsigned();
+            $table->foreign('type_id')->references('id')->on('page_types')->onDelete('cascade');
+            $table->string('name');
+            $table->string('specialities');
+            $table->string('headquarter');
+            $table->longText('content');
+            $table->string('page_profile_image')->nullable();
+            $table->string('page_cover_image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('pages');
+    }
+}
---
+++ database/migrations/2016_03_10_223806_create_pages_admins_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePagesAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('page_admins', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('page_id')->unsigned();
+            $table->foreign('page_id')->references('id')->on('pages')->onDelete('cascade');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('page_admins');
+    }
+}
