fd07b5f8b2139713ffd343d5e41bdc982ebdd1af
Vishwas Vyas <vyasvishwas@github.com>
2016-03-11 21:19:44 +0530

    workstation page frontend version 1 done

---
+++ public/js/showPage.js
@@ -0,0 +1,22 @@
+
+
+$(document).ready(function(){
+
+    $(".main_news_wrapper_outer_1").show();
+    $(".main_news_wrapper_outer_2").hide();
+
+    $(".tab_click1").click(function(){
+        $(".main_news_wrapper_outer_2").fadeOut("1","swing");
+        $(".main_news_wrapper_outer_1").fadeIn("1","swing");
+        $(".tab_item1").addClass("active_tab");
+        $(".tab_item2").removeClass("active_tab");
+        $(".tab_item1").removeClass("deactive_tab");
+    });
+    $(".tab_click2").click(function(){
+        $(".main_news_wrapper_outer_2").fadeIn("1","swing");
+        $(".main_news_wrapper_outer_1").fadeOut("1","swing");
+        $(".tab_item2").addClass("active_tab");
+        $(".tab_item1").removeClass("active_tab");
+        $(".tab_item1").addClass("deactive_tab");
+    });
+});
